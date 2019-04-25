e06cec23111eaadf06f16dfc0834e10b2117674e
Carsten Düllmann <cadull@microsoct.com>
2019-04-25 11:46:17 +0200

    initial

---
+++ 03_InExternalScriptFile/siteScripts.js
@@ -0,0 +1,3 @@
+function incrementNumber() {
+    document.getElementById("target").innerText = Number(document.getElementById("target").innerText) + 1;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+https://www.w3schools.com/Tags/tag_script.asp
+https://www.w3schools.com/js/js_conventions.asp
